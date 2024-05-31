a5298f8bdd28374575c0c5897f2e43f81f7cead2
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-05-31 16:12:10 -0600

    add account key service

---
+++ spec/services/account_key_service_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe AccountKeyService do
+  describe '#call' do
+    subject { described_class.new.call(email, key) }
+  
+    let(:api_url) { ENV['ACCOUNT_KEY_URL'] }
+    let(:email) { Faker::Internet.email }
+    let(:key) { Faker::Internet.password }
+    let(:request_headers) { { 'Content-Type' => 'application/json' } }
+    let(:request_body) { { email: email, key: key }.to_json }
+
+    context 'when the account key api responds successfully' do
+      let(:response_account_key) { Faker::Internet.password }
+      let(:response_body) do
+        {
+          email: email,
+          account_key: response_account_key
+        }.to_json
+      end
+
+      before do
+        stub_request(:post, api_url)
+          .with(body: request_body, headers: request_headers)
+          .to_return(status: 200, body: response_body)
+      end
+
+      it 'returns the account key' do
+        expect(subject).to eq(response_account_key)
+      end
+    end
+
+    context 'when the account key api responds status error' do
+      before do
+        stub_request(:post, api_url)
+          .with(body: request_body, headers: request_headers)
+          .to_return(status: 500)
+      end
+
+      it 'raises an error' do
+        expect { subject }.to raise_error(RestClient::Exception)
+      end
+    end
+  end
+end
