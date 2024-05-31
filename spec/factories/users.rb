ce74273e43e9ed0e9cff58af01e88cad0b3b5d9c
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-05-31 15:10:21 -0600

    add with_metadata trait for users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,5 +6,9 @@ FactoryBot.define do
     phone_number { Faker::PhoneNumber.unique.phone_number }
     full_name { Faker::Name.unique.name }
     password { Faker::Internet.password(min_length: 8, max_length: 15) }
+
+    trait :with_metadata do
+      metadata { "age #{Faker::Number.between(from: 18, to: 65)}, unemployed" }
+    end
   end
 end
