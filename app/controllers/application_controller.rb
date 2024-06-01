72de16c59cfccd69d29b25d8b6bb56985e395a08
Roberto Carrillo <roberto_car_av@hotmail.com>
2024-05-31 20:49:13 -0600

    handle status 500 errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,21 @@
 
 class ApplicationController < ActionController::API
   include JsonResponse
+
+  rescue_from StandardError, with: :handle_standard_error
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
+  private
+
+  def handle_record_not_found(exception)
+    Rails.logger.error(exception)
+    render json: { error: 'Record not found' }, status: :not_found
+  end
+
+  def handle_standard_error(exception)
+    Rails.logger.error(exception.message)
+    Rails.logger.error(exception.backtrace.join("\n"))
+
+    render json: { error: 'Internal server error' }, status: :internal_server_error
+  end
 end
